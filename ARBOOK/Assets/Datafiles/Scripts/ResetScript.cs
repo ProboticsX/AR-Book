a4138705e7722e4dfe48007b711768565557ec3d
shubham jain <36927669+ProboticsX@users.noreply.github.com>
2020-10-25 17:07:29 +0530

    initial commit

---
+++ ARBOOK/Assets/Datafiles/Scripts/ResetScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetScript : MonoBehaviour {
+	Vector3 originalpos;
+	// Use this for initialization
+	void Start () {
+		originalpos = transform.localPosition;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider collide){
+		if (collide.gameObject.CompareTag ("SHIPWALL")) {
+			transform.localPosition = originalpos;
+		}
+		else if (collide.gameObject.CompareTag ("AEROPLANEWALL")) {
+			transform.localPosition = originalpos;
+		}
+		else if (collide.gameObject.CompareTag ("CLOUDWALL")) {
+			transform.localPosition = originalpos;
+		}
+	}
+}
